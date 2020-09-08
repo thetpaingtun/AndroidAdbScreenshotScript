2b1a499eec64a2081d249e5054687708c21d689f
ThetPaing Tun <thetpaingtun93@gmail.com>
2020-09-08 15:08:34 +0800

    updated animation.sh

--- mac/animation.sh
+++ mac/animation.sh
@@ -1,4 +1,15 @@
-adb shell settings put global window_animation_scale 10
-adb shell settings put global transition_animation_scale 10
-adb shell settings put global animator_duration_scale 10
+speed=$1
 
+# echo $speed
+
+if [[ -z $speed ]]; then
+	speed=1
+fi
+
+
+adb shell settings put global window_animation_scale $speed
+adb shell settings put global transition_animation_scale $speed
+adb shell settings put global animator_duration_scale $speed
+
+
+echo "animation scale changed to : $speed"
